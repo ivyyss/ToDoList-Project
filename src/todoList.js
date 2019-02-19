38c5aca48f66e583eaa4414f299375cfe950335a
ivyyss <nameyss144@163.com>
2019-02-19 16:51:11 +0800

    change input into todoinput props

--- src/todoList.js
+++ src/todoList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodoInput from './todoInput.js'
 
 class Application extends React.Component {
     constructor (props){
@@ -17,7 +18,7 @@ class Application extends React.Component {
             <div className='application'>
                 <h1>我的待办</h1>
                 <div className='inputWrapper' >
-                    <input type='text' value={this.state.newTodo} />
+                    <TodoInput content={this.state.newTodo} />
                 </div>
                 <ul>
                     {todos}
