edb8f60037645cbaee45d55c0138e948cb6e5ee2
ivyyss <nameyss144@163.com>
2019-02-26 18:50:55 +0800

    make leanCloud.js

---
+++ src/leanCloud.js
@@ -0,0 +1,10 @@
+import AV from 'leancloud-storage';
+
+var APP_ID = 'BwlLHpUypqlQQTDTQarqgORI-gzGzoHsz';
+var APP_KEY = '0EekaQvmuWuXdWhzo51gfRQR';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+})
+
+export default AV;
