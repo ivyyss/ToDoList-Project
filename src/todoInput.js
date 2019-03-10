2bf7b37b5db4c5cb82ba41ad1a97703c42173fe5
ivyyss <nameyss144@163.com>
2019-03-10 15:17:40 +0800

    refuse submit if enter empty content

--- src/todoInput.js
+++ src/todoInput.js
@@ -6,8 +6,10 @@ export default class TodoInput extends React.Component {
         return <input type='text' placeholder="Type your plan here!" value={this.props.content} onKeyPress={this.submit.bind(this)} onChange={this.changeTitle.bind(this)}/>
     }
     submit (e){
-        if(e.key==='Enter') {  
-            this.props.onSubmit(e)
+        if(e.key==='Enter') {
+            if(e.target.value.trim()!==''){
+                this.props.onSubmit(e)
+            }
         }     
     }
     changeTitle(e){
