470a1d17d30e98720c7cf0a1d769758a6cf600c2
ivyyss <nameyss144@163.com>
2019-03-12 18:39:08 +0800

    Updates

---
+++ precache-manifest.3229e4a14effbde7e856e2ca4e8e97ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa3e306a3d9350f48386",
+    "url": "/ToDoList-Project/build/static/js/runtime~main.aa3e306a.js"
+  },
+  {
+    "revision": "87c8e50be5c82e4d68f1",
+    "url": "/ToDoList-Project/build/static/js/main.87c8e50b.chunk.js"
+  },
+  {
+    "revision": "b280a46fbb2e8feb7269",
+    "url": "/ToDoList-Project/build/static/js/2.b280a46f.chunk.js"
+  },
+  {
+    "revision": "87c8e50be5c82e4d68f1",
+    "url": "/ToDoList-Project/build/static/css/main.e9ed4663.chunk.css"
+  },
+  {
+    "revision": "b280a46fbb2e8feb7269",
+    "url": "/ToDoList-Project/build/static/css/2.2f4903e5.chunk.css"
+  },
+  {
+    "revision": "b5b1b493c1262eefab03e31504719753",
+    "url": "/ToDoList-Project/build/index.html"
+  }
+];
